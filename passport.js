1a50836ff76158f3360a93e61b0875896a80f8f0
shadesi <45621420+shadesi@users.noreply.github.com>
2024-08-21 23:08:34 +0300

    Update passport.js
    updated

--- passport.js
+++ passport.js
@@ -2,34 +2,43 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const passportJWT = require('passport-jwt');
 const Models = require('./models.js');
+const express = require('express');
+const app = express(); 
 
 let Users = Models.User;
 let JWTStrategy = passportJWT.Strategy;
 let ExtractJWT = passportJWT.ExtractJwt;
 
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: 'Username',
-      passwordField: 'Password',
-    },
-    async (username, password, done) => {
-      try {
-        const user = await Users.findOne({ Username: username });
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username or password.' });
-        }
-        if (user.Password !== password) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
-        return done(null, user);
-      } catch (error) {
-        return done(error);
+// Define the /users route
+app.post('/users', async (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+    .then((user) => {
+      if (user) {
+        // If the user is found, send a response that it already exists
+        return res.status(400).send(req.body.Username + ' already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) => { res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
       }
-    }
-  )
-);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
 
+// Configure Passport to use JWT strategy
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: 'your_jwt_secret' // Ensure this matches the secret used in auth.js
